e2e4a135e85f9541e8c3ac65e7d552a32284fd74
MaslovaOleksandra <159123855+MaslovaOleksandra@users.noreply.github.com>
2025-01-22 22:47:11 +0100

    add_row

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Vytvořte dvourozměrné pole, ve kterém budou 3 řádky a 3 sloupce s libovolnými čísly
-pole = array([[16, 72, 48],
+pole = np.array([[16, 72, 48],
             [94, 35, 61],
             [77, 80, 59]])
 
@@ -7,5 +7,13 @@ print(pole)
 
 #V již vytvořeném dvourozměrném poli změňte na druhém řádku a druhé sloupci vaše číslo na číslo 105
 pole[35] = 105
+
+print(pole)
+
+#Přidejte řádek a sloupec ve vašem dvourozměrném poli.
+novy_radek = np.array([10, 21, 99])
+novy_sloupec = np.array([[7], [44], [1], [67]])
+pole = np.append(pole, [novy_radek], axis=0)
+pole = np.append(pole, novy_sloupec, axis=1)
 
 print(pole)
